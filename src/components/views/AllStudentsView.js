efee4725ae0bf1bf962c1a7a5523a86364aaae35
Aleena Thomas <aleenathomas@Aleenas-MBP.lan>
2021-05-13 21:05:08 -0400

    duplicated layout of all students to match all campuses

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -1,13 +1,125 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import './header.css';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+    fontType: 'bold',
+    fontFamily: 'Courier, sans-serif', 
+    fontSize: '35px', 
+    color: '#CDDC39'
+  },
+  appBar:{
+    backgroundColor: '#11153e',
+    shadows: ['none'],
+  },
+  greeting:{
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    width: "50%",
+    margin: "auto",
+  },
+  links:{
+    textDecoration: 'none',
+  }
+
+}));
 
 const AllStudentsView = (props) => {
+  const classes = useStyles();
   if (!props.allStudents.length) {
-    return <div>There are no students.</div>;
+    return <div>
+      <h1 class="centerheader">Student Listing</h1>
+      <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+          {/* <Typography variant="h6" className={classes.title} color="inherit" >
+            CRUD App
+          </Typography> */}
+          <Typography className={classes.title}>
+            <Link className={classes.links} to={'/'} >
+              <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+                Home
+              </Button>
+            </Link>
+          </Typography>
+
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              Campuses
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
+              Students
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <div class="centerheader">
+        <h1> There are no students registered in the database.</h1>
+        <Link className={classes.links} to={'/'} >
+            <Button variant="contained" color="primary">
+              Add Campus
+            </Button>
+        </Link>
+      </div>
+    </div>;
   }
 
   return (
     <div>
+      <h1 class="centerheader">Student Listing</h1>
+      <AppBar position="static" elevation={0} className={classes.appBar}>
+        <Toolbar>
+          {/* <Typography variant="h6" className={classes.title} color="inherit" >
+            CRUD App
+          </Typography> */}
+          <Typography className={classes.title}>
+            <Link className={classes.links} to={'/'} >
+              <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+                Home
+              </Button>
+            </Link>
+          </Typography>
+
+          <Link className={classes.links} to={'/campuses'} >
+            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+              Campuses
+            </Button>
+          </Link>
+
+          <Link className={classes.links} to={'/students'} >
+            <Button variant="contained" color="primary">
+              Students
+            </Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <div className='row'>
+        <div className="columnleft">
+          <h1>All Students</h1>
+        </div>
+        <div className="columnright">
+          <Link className={classes.links} to={'/'} >
+              <Button variant="contained" color="primary">
+                Add Student
+              </Button>
+          </Link>
+        </div>
+      </div>
       {props.allStudents.map((student) => (
         <div key={student.id}>
           <Link to={`/student/${student.id}`}>
